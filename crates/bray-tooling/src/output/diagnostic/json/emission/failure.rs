f84384fa4134a3732708c6dd85389de1348e4ba5
Anton B <antonberlin@gmail.com>
2026-09-04 23:36:15 +0200

    style: apply rust formatting

--- crates/bray-tooling/src/output/diagnostic/json/emission/failure.rs
+++ crates/bray-tooling/src/output/diagnostic/json/emission/failure.rs
@@ -154,10 +154,7 @@ pub(super) fn count_u64_field(name: &'static str, value: u64) -> DiagnosticEmiss
     field(name, DiagnosticEmissionFieldValueJson::Count(value))
 }
 
-pub(super) fn count_usize_field(
-    name: &'static str,
-    value: usize,
-) -> DiagnosticEmissionFieldJson {
+pub(super) fn count_usize_field(name: &'static str, value: usize) -> DiagnosticEmissionFieldJson {
     count_u64_field(name, u64::try_from(value).unwrap_or(u64::MAX))
 }
 
